f5fb302e7e1fec598b842efdc767018ceb48b698
Joao Pereira <joao.090405@bento.local>
2022-10-04 15:45:44 -0300

    inclusao carbon e migrations

---
+++ Magazine_Joao/database/migrations/2022_10_04_182940_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->string("descricao");
+            $table->string("marca");
+            $table->integer("estoque");
+            $table->float("preco");
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->string("imagem"); //aqui vem o link para a imagem
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
