bb1d192a7218e1949965039cee00bc164de29569
Joao Pereira <joao.090405@bento.local>
2022-10-25 17:21:24 -0300

    crud produtos

--- Magazine_Joao/app/Http/Controllers/ProdutoController.php
+++ Magazine_Joao/app/Http/Controllers/ProdutoController.php
@@ -9,42 +9,66 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    function index(Request $request, $id){ //tela com a descrição dos produtos
+    function index(Request $request, $id, $id_filho)
+    { //tela com a descrição dos produtos
         $produto = Produto::select('*')->where('id', $id)->get();
-        $produto_filho = ProdutoFilho::select('*')->where('id_pai', $id)->get();
+        $produtos_filho = ProdutoFilho::select('*')->where('id_pai', $id)->get();
+
 
         return view('home.index', [
             'produto' => $produto,
-            'produtos_filho' => $produto_filho,
+            'produtos_filho' => $produtos_filho,
         ]);
     }
 
-    function update(Request $request, Produto $produto){
+    function update(Request $request, Produto $produto, ProdutoFilho $produto_filho)
+    {
         $data = $request->all();
 
         $produto->fill($data);
         $produto->save();
 
+        if (isset($data['variacao'])) {
+            $produto_filho->variacao = $data['variacao'];
+            $produto_filho->save();
+            // $produto_filho->update(['variacao' => $data['variacao']]);
+
+        }
+        if (isset($data['estoque'])) {
+            $produto_filho->estoque = $data['estoque'];
+            $produto_filho->save();
+        }
+
         return "ok";
     }
 
-    function destroy_filho(ProdutoFilho $produto_filho){
+    function deactivate_child(Produto $produto, ProdutoFilho $produto_filho)
+    {
+        $produto_id = $produto->select("id")->first();
+        try {
+            $produto_filho->update(['active' => 0]);
+            return "ok";
+            return redirect("/produto/$produto_id");
+        } catch (QueryException $e) {
+            return "EEEEEE" . $e;
+            return redirect("/produto/$produto_id/$produto_filho")->with("erro", "Erro!");
+        }
     }
 
-    function deactivate(Produto $produto){
+    function deactivate(Produto $produto, $produto_filho)
+    {
         try {
-            $produto->update(['active' => 0]);
+            $produto->update(['active' => 0, 'categoria_id' => 1]);
             return "ok";
-            return redirect ("/");
-        } catch (QueryException $e){
+            return redirect("/");
+        } catch (QueryException $e) {
             $produto_id = $produto->select("id")->first();
             return "EEEEEE" . $e;
-            return redirect("/produto/$produto_id")->with("erro", "Erro! Há produtos nesta categoria!");
+            return redirect("/produto/$produto_id/$produto_filho")->with("erro", "Erro");
         }
-
     }
 
-    function add_child(){
-
+    function add_child()
+    {
     }
 }
