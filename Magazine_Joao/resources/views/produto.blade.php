d786b2d30c07e7e751bf01ba1b76bb1c0f0d8bef
João Vitor Angelo Pereira <jvangelopereira@gmail.com>
2022-11-30 17:15:04 -0300

    avanço na tela de produto

--- Magazine_Joao/resources/views/produto.blade.php
+++ Magazine_Joao/resources/views/produto.blade.php
@@ -2,92 +2,99 @@
     <div class="row g-4">
 
         @php
-        $disabled = 'disabled';
-        $produto = [
-            'id' => 1,
-            'nome' => 'Geladeira Brastemp',
-            'preco' => '5990.90',
-            'imagem' => asset('/img/fogo-no-fogão-de-gás-sujo-da-cozinha-137827376.jpg')
-        ]
+            $disabled = 'disabled';
+            $produto = [
+                'id' => 1,
+                'nome' => 'Geladeira Brastemp',
+                'preco' => '5990.90',
+                'imagem' => asset('/img/fogo-no-fogão-de-gás-sujo-da-cozinha-137827376.jpg'),
+                'estoque' => 24,
+            ];
         @endphp
-        <div class="col-4 text-center img-produto">
-            <img src="{{$produto['imagem']}}" class="img-fluid foto-produto">
+        <div class="prod-info-esquerda">
+            <div class="img-produto-info text-center img-produto">
+                <img src="{{ $produto['imagem'] }}" class="img-fluid foto-produto">
+            </div>
+            <div class="filhos">
+                <button type="button" value="1" class="btn-filho">P</button>
+                <button type="button" value="2" class="btn-filho">M</button>
+                <button type="button" value="3" class="btn-filho">G</button>
+            </div>
         </div>
-        <div class="col-6">
-            <form action="/produto/update">
-            @include('components.input', [
-            'type' => 'hidden',
-            'class' => '',
-            'placeholder' => '',
-            'value' => '',
-            'name' => 'id',
-            'disabled' => $disabled
-            ])
-            @include('components.input', [
-            'type' => 'hidden',
-            'class' => '',
-            'placeholder' => '',
-            'value' => '',
-            'name' => 'id-filho',
-            'disabled' => $disabled
-            ])
-            @include('components.input', [
-            'type' => 'text',
-            'class' => 'input-produto',
-            'placeholder' => 'Nome do Produto',
-            'value' => '',
-            'name' => 'titulo-produto',
-            'disabled' => $disabled
-            ])
-            @include('components.input', [
-            'type' => 'text',
-            'class' => 'input-produto',
-            'placeholder' => 'Marca do Produto',
-            'value' => '',
-            'name' => 'marca-produto',
-            'disabled' => $disabled
-            ])
-            @include('components.textarea', [
-            'type' => 'textarea',
-            'class' => 'input-produto',
-            'placeholder' => 'Descrição do Produto',
-            'value' => '',
-            'name' => 'descricao-produto',
-            'disabled' => $disabled
-            ])
-            @include('components.input', [
-            'type' => 'number',
-            'class' => 'input-produto',
-            'placeholder' => 'Preço do Produto',
-            'value' => '',
-            'name' => 'preco-produto',
-            'disabled' => $disabled
-            ])
-            <label for="quantidade">Quantidade: </label>
-            @include('components.input', [
-            'type' => 'number',
-            'class' => 'input-produto',
-            'placeholder' => '',
-            'value' => '',
-            'name' => 'quantidade-produto',
-            'disabled' => $disabled
-            ])
-            @include('components.input', [
-            'type' => 'submit',
-            'class' => 'input-produto',
-            'placeholder' => '',
-            'value' => 'Alterar Produto',
-            'name' => 'preco-produto',
-            'disabled' => ''
-            ])
-            <a href="#" class="btn btn-laranja"><i class="bi bi-cart-plus-fill font-size-25"></i></a>
-            <button type="button" value="1" class="btn">P</button>
-            <button type="button" value="2" class="btn">M</button>
-            <button type="button" value="3" class="btn">G</button>
-            <button type="button">Comprar</button>
-            <p>Quantidade em estoque: 32</p>
-            <a href="#" class="btn btn-laranja"><i class="bi bi-trash-fill font-size-25"></i></a>
-            </form>
+        <div class="prod-info-direita">
+            <div class="info-produto">
+                <form action="/produto/update">
+                    @include('components.input', [
+                        'type' => 'hidden',
+                        'class' => '',
+                        'placeholder' => '',
+                        'value' => '',
+                        'name' => 'id',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.input', [
+                        'type' => 'hidden',
+                        'class' => '',
+                        'placeholder' => '',
+                        'value' => '',
+                        'name' => 'id-filho',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.input', [
+                        'type' => 'text',
+                        'class' => 'input-produto',
+                        'placeholder' => 'Nome do Produto',
+                        'value' => '',
+                        'name' => 'titulo-produto',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.input', [
+                        'type' => 'text',
+                        'class' => 'input-produto',
+                        'placeholder' => 'Marca do Produto',
+                        'value' => '',
+                        'name' => 'marca-produto',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.textarea', [
+                        'type' => 'textarea',
+                        'class' => 'input-produto',
+                        'placeholder' => 'Descrição do Produto',
+                        'value' => '',
+                        'name' => 'descricao-produto',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.input', [
+                        'type' => 'number',
+                        'class' => 'input-produto',
+                        'placeholder' => 'Preço do Produto',
+                        'value' => '',
+                        'name' => 'preco-produto',
+                        'disabled' => $disabled,
+                    ])
+                    <label for="quantidade">Quantidade: </label>
+                    @include('components.input', [
+                        'type' => 'number',
+                        'class' => 'input-produto',
+                        'placeholder' => '',
+                        'value' => '',
+                        'name' => 'quantidade-produto',
+                        'disabled' => $disabled,
+                    ])
+                    @include('components.input', [
+                        'type' => 'submit',
+                        'class' => 'input-produto',
+                        'placeholder' => '',
+                        'value' => 'Alterar Produto',
+                        'name' => 'preco-produto',
+                        'disabled' => '',
+                    ])
+                    <a href="#" class="btn btn-laranja"><i class="bi bi-cart-plus-fill font-size-25"></i></a>
+                    <button type="button">Comprar</button>
+                    <p>Quantidade em estoque: {{ $produto['estoque'] }}</p>
+                    <a href="#" class="btn btn-laranja"><i class="bi bi-trash-fill font-size-25"></i></a>
+            </div>
         </div>
+        </form>
     </div>
 </div>
