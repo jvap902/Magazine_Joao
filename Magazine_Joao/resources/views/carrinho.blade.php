7da6658d1a59607a4faa02410fc2abeb1ec6a4b5
João Victor Trindade <jv.trindadesilva@gmail.com>
2022-12-02 03:24:31 -0300

    Tela do carrinho visualmente pronta

--- Magazine_Joao/resources/views/carrinho.blade.php
+++ Magazine_Joao/resources/views/carrinho.blade.php
@@ -1,17 +1,42 @@
+@php
+$produtos = [
+    'produto1' => [
+        'nome' => 'Camisa Barcelona Home 2022/23',
+        'qtd' => 'Qtde: 1',
+        'valor' => 'R$359,90'
+    ],
+    'produto2' => [
+        'nome' => 'Conjunto de Facas Tramontina Nome Super Ultra Longo',
+        'qtd' => 'Qtde: 3',
+        'valor' => 'R$10599,90'
+    ]
+];
+
+$valor = 649.99
+@endphp
 <div class="d-flex align-items-center justify-content-center telaLogin">
-    <div class="login d-flex align-items-center justify-content-center borda-laranja borda-form">
-        <div class="form">
-            <form name="criarConta" action="/usuarios/store" method="POST">
+    <div class="carrinho d-flex align-items-center justify-content-center borda-laranja borda-form">
+        <div class="dadosCarrinho">
+            <form name="carrino" action="VER" method="POST">
                 <div name="dadosUsuario">
                     <h2>Dados do Usuário</h2>
-                    <p class="dadoUsuario">João Victor Freitas Batistela Trindade da Silva</p> <!-- tem que ver como receber do Backend -->
-                    <p class="dadoUsuario">CPF: 000.000.000-00</p> <!-- tem que ver como receber do Backend -->
+                    <p class="dadoCarrinho">João Victor Freitas Batistela Trindade da Silva</p> <!-- tem que ver como receber do Backend -->
+                    <p class="dadoCarrinho">CPF: 000.000.000-00</p> <!-- tem que ver como receber do Backend -->
                 </div>
                 <div name="dadosCompra">
-
+                    <h2>Informações da Compra</h2>
+                    <h1 class="valorTotal">Valor Total: R$ {{ $valor }}</h1>
+                    @foreach($produtos as $produto)
+                    @include('components.infoProdutoCarrinho', [
+                        'produto' => $produto
+                    ])
+                    @endforeach
                 </div>
                 <div name="acoes">
-
+                <button type="button" class="btn-submit">Cancelar</button>
+                <button type="button" class="btn-submit btn-carrinho">Gerar Boleto</button>
+                <button type="button" class="btn-submit btn-carrinho">PIX</button>
+                <h3 class="btn-carrinho">Confirmar compra:</h3>
                 </div>
             </form>
         </div>
---
+++ Magazine_Joao/resources/views/components/infoProdutoCarrinho.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div class= "infoProdutoCarrinho borda-laranja borda-form">
+    <a href="#" class="d-flex align-items-center justify-content icone icone-carrinho"><i class="bi bi-trash-fill laranja font-size-25"></i></a>
+        <p class="dadoCarrinho dadoCarrinhoTitulo">{{ $produto['nome'] }}</p>
+        <p class="dadoCarrinho">{{ $produto['qtd'] }}</p>
+        <p class="dadoCarrinho">{{ $produto['valor'] }}</p>
+    </div>
+</div>
